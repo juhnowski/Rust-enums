e7d465561e490a01104785c0d2d6b9fdd71d3bd5
Juhnowski <juhhnowski@gmail.com>
2018-09-11 00:25:11 +0300

    multitype enum

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,26 @@ enum IPAddrType {
         V6(String),
 }
 
+enum Message {
+    Quit,
+    Move{x:i32, y:i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self){
+        println!("I'm inside call");
+    }
+}
+
 fn main() {
     let four = IPAddrType::V4;
     let six = IPAddrType::V6;
 
     let home = IPAddrType::V4(String::from("127.0.0.1"));
     let loopback = IPAddrType::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
